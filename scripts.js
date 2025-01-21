58b646b1b7dbc987abb386144ca027f0a5890381
Ethan Cao <ethancao0104@gmail.com>
2025-01-21 02:12:28 -0800

    Added mathjs library and completed calculator functionality

--- scripts.js
+++ scripts.js
@@ -15,60 +15,86 @@ const addBtn = document.querySelector('.add');
 
 addBtn.addEventListener('click', () => {
   //add a + symbol to the display
-  display.innerHTML += " + ";
+  if (display.innerHTML === "") {
+    display.innerHTML = "ERROR"
+    return null;
+  } else if (display.innerHTML === "ERROR" || display.innerHTML ===  "Infinity") {
+    return null;
+  }
+  display.innerHTML += " + "
 })
-
-function add(x, y) {
-  console.log(x+y)
-  return x + y;
-}
 
 //FUNCTIONALITY FOR SUBTRACT BUTTON
 const subtractBtn = document.querySelector('.subtract');
 
 subtractBtn.addEventListener('click', () => {
   // add a - symbol to the display to be parsed
+  if (display.innerHTML === "") {
+    display.innerHTML = "ERROR"
+    return null;
+  } else if (display.innerHTML === "ERROR" || display.innerHTML ===  "Infinity") {
+    return null;
+  }
   display.innerHTML += " - ";
 })
-
-function subtract(x, y) {
-  console.log(x-y)
-  return x - y;
-}
 
 //FUNCTIONALITY FOR MULTIPLY BUTTON
 const multiplyBtn = document.querySelector('.multiply');
 
 multiplyBtn.addEventListener('click', () => {
   // add a * symbol to the display to be parsed
+  if (display.innerHTML === "") {
+    display.innerHTML = "ERROR"
+    return null;
+  } else if (display.innerHTML === "ERROR" || display.innerHTML ===  "Infinity") {
+    return null;
+  }
   display.innerHTML += " * ";
 })
-
-function multiply(x, y) {
-  console.log(x*y)
-  return x * y;
-}
 
 //FUNCTIONALITY FOR DIVIDE BUTTON
 const divideBtn = document.querySelector('.divide');
 
 divideBtn.addEventListener('click', () => {
   // add a / symbol to the display to be parsed
+  if (display.innerHTML === "") {
+    display.innerHTML = "ERROR"
+    return null;
+  } else if (display.innerHTML === "ERROR" || display.innerHTML ===  "Infinity") {
+    return null;
+  }
   display.innerHTML += " / ";
 })
-
-function divide(x, y) {
-  console.log(x/y)
-  return x/y;
-}
 
 // Making the number buttons add the numbers into the display
 const numberBtn = document.querySelectorAll('.number');
 
 numberBtn.forEach(button => {
   button.addEventListener('click', event => {
+    if (display.innerHTML[0] == "0") {
+      display.innerHTML = "ERROR";
+      return null;
+    } else if (display.innerHTML === "ERROR" || display.innerHTML === "Infinity") {
+      return null;
+    }
     display.innerHTML += button.innerHTML;
   })
+})
+
+// Creating the clear button function
+const clearBtn = document.querySelector('.clear');
+
+clearBtn.addEventListener('click', () => {
+  display.innerHTML = "";
+})
+
+// FUNCTIONAlITY FOR OUT OPERATOR
+const calculate = document.querySelector('.operator');
+
+calculate.addEventListener('click', () => {
+  let equation = display.innerHTML;
+  const result = math.evaluate(equation);
+  display.innerHTML = result;
 })
 
 
